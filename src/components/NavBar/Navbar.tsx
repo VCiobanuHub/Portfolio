cbaccaf1fbda1bb0aae94c4b8572e43aa1082952
Victor Ciobanu <victorciobanu95@hotmail.com>
2022-08-09 19:24:06 +0100

    main

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,7 +1,22 @@
 import "./Navbar.scss";
+import { images } from "../../constants";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} />
+      </div>
+      <ul className="app__navbar-link">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
