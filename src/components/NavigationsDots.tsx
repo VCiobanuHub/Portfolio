39f94776c954c717c7bfae15883871171009a70a
Victor Ciobanu <victorciobanu95@hotmail.com>
2022-08-09 23:38:26 +0100

    main

---
+++ src/components/NavigationsDots.tsx
@@ -0,0 +1,18 @@
+function NavigationsDots({ active }) {
+  return (
+    <div className="app__navigation">
+      {["home", "about", "work", "skills", "testimonials", "contact"].map(
+        (item, index) => (
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={active === item ? { backgroudColor: "#313BAC" } : {}}
+          />
+        )
+      )}
+    </div>
+  );
+}
+
+export default NavigationsDots;
--- src/container/About/About.tsx
+++ src/container/About/About.tsx
@@ -1,7 +1,52 @@
 import "./About.scss";
 
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
+
+import { urlFor, client } from "../../client";
+
 function About() {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      console.log({ data });
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">
+        I Know That <span>Good Apps</span>
+        <br /> means <span> Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default About;
+export default AppWrap(About, "about");
